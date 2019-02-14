f5f016e3bfabace5cac77009e740108fc2c8ca10
cybniv <5002075+cybniv@users.noreply.github.com>
2019-02-14 13:03:52 +0100

    update transparency function

--- functions.el
+++ functions.el
@@ -56,18 +56,11 @@
 (defun tramp-set-auto-save ()
   (auto-save-mode -1))
 
-;; (defun toggle-transparency ()
-;;   (interactive)
-;;   (let ((alpha (frame-parameter nil 'alpha)))
-;;     (set-frame-parameter
-;;      nil 'alpha
-;;      (if (eql (cond ((numberp alpha) alpha)
-;;                     ((numberp (cdr alpha)) (cdr alpha))
-;;                     ;; Also handle undocumented (<active> <inactive>) form.
-;;                     ((numberp (cadr alpha)) (cadr alpha)))
-;;               100)
-;;          '(97 . 50) '(100 . 100)))))
-
+ ;; Set transparency of emacs
+ (defun transparency (value)
+   "Sets the transparency of the frame window. 0=transparent/100=opaque"
+   (interactive "nTransparency Value 0 - 100 opaque:")
+   (set-frame-parameter (selected-frame) 'alpha value))
 
 (defun eshell-new()
   "Open a new instance of eshell."
