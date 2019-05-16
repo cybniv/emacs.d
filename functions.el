40ccb727034aaff8566596629a2ddf38eda69206
cybniv <5002075+cybniv@users.noreply.github.com>
2019-05-16 20:16:41 +0200

    minor indentation edit

--- functions.el
+++ functions.el
@@ -57,10 +57,10 @@
   (auto-save-mode -1))
 
  ;; Set transparency of emacs
- (defun transparency (value)
-   "Sets the transparency of the frame window. 0=transparent/100=opaque"
-   (interactive "nTransparency Value 0 - 100 opaque:")
-   (set-frame-parameter (selected-frame) 'alpha value))
+(defun transparency (value)
+  "Sets the transparency of the frame window. 0=transparent/100=opaque"
+  (interactive "nTransparency Value 0 - 100 opaque:")
+  (set-frame-parameter (selected-frame) 'alpha value))
 
 (defun eshell-new()
   "Open a new instance of eshell."
