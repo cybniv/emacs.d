7b41d32a83c0667aab5b07676b584ececcc304cc
cybniv <5002075+cybniv@users.noreply.github.com>
2019-06-14 15:38:08 +0200

    fix use-package config of org-journal | add doom-modeline

--- init_packages.el
+++ init_packages.el
@@ -1,6 +1,11 @@
 (use-package org-journal
   :ensure t
+  :after org
   :defer t
   :custom
   (org-journal-dir "~/Org/journal/")
   (org-journal-date-format "%A, %d %B %Y"))
+
+(use-package doom-modeline
+  :ensure t
+  :hook (after-init . doom-modeline-mode))
