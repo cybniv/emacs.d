acf6b942c037904512fd925d0e4a0e4c5d39e27e
cybniv <5002075+cybniv@users.noreply.github.com>
2019-08-22 00:26:45 +0200

    keys.el: Cleanup

--- keys.el
+++ keys.el
@@ -1,15 +1,11 @@
-;; define keys
 (global-unset-key [kp-insert])
 (global-unset-key [insert])
 (global-unset-key (kbd "C-t"))
-(global-unset-key (kbd "C-x c"))
 (global-unset-key (kbd "C-z"))
 (global-unset-key (kbd "M-d"))
 (global-unset-key (kbd "C-<delete>"))
 (global-unset-key (kbd "M-k"))
 ;(global-unset-key (kbd "C-w"))
-
-(global-set-key (kbd "C-z") 'yank)
 
 (global-set-key [f2] 'undo)
 (global-set-key [f3] 'helm-find-files)
@@ -35,16 +31,13 @@
 (global-set-key (kbd "C-x C-r") 'helm-recentf)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "M-g") 'goto-line)
-
-(global-set-key (kbd "s-m") 'mu4e)
-
 (global-set-key (kbd "C-c g g") 'browse-at-remote)
 ;; helm
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
 ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
 ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
 
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
@@ -52,19 +45,13 @@
 
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+(global-set-key (kbd "<menu>") 'helm-M-x)
 (global-set-key (kbd "M-x") 'helm-M-x)
+
 (global-set-key (kbd "C-x r b") 'helm-bookmarks)
 
 ; (global-set-key (kbd "C-c h o") 'helm-occur)
 ; (global-set-key (kbd "C-c h g") 'helm-google-suggest)
 
-(global-set-key (kbd "s-<left>") 'previous-buffer)
-(global-set-key (kbd "s-<right>") 'next-buffer)
-
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-
-(global-set-key (kbd "s-<enter>") #'terminal-here-project-launch)
-; (global-set-key (kbd "s-<enter>") #'terminal-here-launch)
-
-(provide 'keys)
-;;; keys.el ends here
+;; (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
