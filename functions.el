2a4780ff98532482c457ddd6199b6ee8c4407fcb
cybniv <5002075+cybniv@users.noreply.github.com>
2019-10-12 15:06:32 +0200

    add copy-file-name-to-clipboard function

--- functions.el
+++ functions.el
@@ -108,3 +108,13 @@
   (forward-line 1)
   (transpose-lines 1)
   (forward-line -1))
+
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
